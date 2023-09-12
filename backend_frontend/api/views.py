b199855c5b0078fba2b0faf43ed5b7a0175f10be
Swastik Verma <swastikverma308@gmail.com>
2023-09-12 11:57:38 +0530

    mongodb->django

--- backend_frontend/api/views.py
+++ backend_frontend/api/views.py
@@ -1,3 +1,40 @@
+import pymongo
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def create_user(request):
+    """
+    Create a new user.
+    """
+    username = request.POST['username']
+    password = request.POST['password']
+
+    client = pymongo.MongoClient('mongodb://localhost:27017')
+    db = client['TempUser']
+
+    # Create a new user document.
+    user = {
+        'username': username,
+        'password': password,
+    }
+
+    db.users.insert_one(user)
+
+    return HttpResponse('User created successfully!')
+
+
+def get_users(request):
+    """
+    Get all users.
+    """
+    client = pymongo.MongoClient('mongodb://localhost:27017')
+    db = client['TempUser']
+
+    # Get all users.
+    users = db.users.find()
+
+    return render(request, 'users.html', {'users': users})
+
+def main(request):
+    print("get main accessed")
+    return HttpResponse('<h1>Hello Http</h1>')
