be13036406a7ae9198f8e4aa3c566517bbb2a9ee
Swastik Verma <swastikverma308@gmail.com>
2023-09-13 19:30:10 +0530

    remived warnings

--- backend_frontend/api/urls.py
+++ backend_frontend/api/urls.py
@@ -3,5 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('create', create_user, name='create_user'),
-    path('',index, name='index')
+    path('',index, name='index'),
+    path('dashboard',dashboard),
+    path('prediction_form', prediction_form, name='prediction_form'),
+    path('prediction_form/save', save_data, name='save_data'),
 ]
--- backend_frontend/api/views.py
+++ backend_frontend/api/views.py
@@ -1,11 +1,61 @@
 import pymongo
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.csrf import csrf_exempt
+
+from django.contrib.auth.decorators import login_required
 
 
+#rendering templates
 def index(request):
-    
     return render(request,  'index.html')
+
+def prediction_form(request):
+    return render(request, '')
+
+
+def dashboard(request):
+    return render(request,'')
+
+
+
+
+@csrf_exempt
+@login_required
+def save_data(request):
+    if request.method == 'POST':
+        blood_group = request.POST['blood_group']
+        work_condition = request.POST['work_condition']
+        city = request.POST['city']
+        age = request.POST['age']
+        image = request.FILES['image']
+        
+        client = pymongo.MongoClient('mongodb://localhost:27017')
+        db = client['TempUser']
+
+        # Get the phone number of the logged in user.
+        phone_number = request.user.phone_number
+
+        # Save the data to the MongoDB database.
+        user_data = {
+            'phone_number': phone_number,
+            'blood_group': blood_group,
+            'work_condition': work_condition,
+            'city': city,
+            'age': age,
+            'image': image,
+        }
+
+        db.user_inputs.insert_one(user_data)
+
+        # Redirect to the dashboard route.
+        return HttpResponseRedirect('/dashboard')
+
+
+
+
+
+
 
 def create_user(request):
     """
